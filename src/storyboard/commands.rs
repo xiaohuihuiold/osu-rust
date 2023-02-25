e6faeebfb160880e0b820c425fd3e9add77a93cc
xiaohuihuiold <xiaohuihuiold@gmail.com>
2023-02-25 23:49:17 +0800

    feat(storyboard): 添加普通指令参数

--- src/storyboard/commands.rs
+++ src/storyboard/commands.rs
@@ -1,25 +1,6 @@
+use crate::graphics::{Color, Curves, Point};
 use deserialize::from_str_enum;
 use std::str::FromStr;
-
-/// 通用参数
-pub struct CommonCommand {}
-
-/// 透明度命令
-pub struct FadeCommand {}
-
-/// 移动命令
-pub struct MoveCommand {}
-pub struct MoveXCommand {}
-pub struct MoveYCommand {}
-
-/// 缩放命令
-pub struct ScaleCommand {}
-
-/// 旋转命令
-pub struct RotateCommand {}
-
-/// 颜色命令
-pub struct ColorCommand {}
 
 from_str_enum! {
     /// 参数命令类型
@@ -30,11 +11,76 @@ from_str_enum! {
     }
 }
 
+/// 通用参数
+pub struct CommonCommand {
+    easing: Curves,
+    start_time: i64,
+    end_time: i64,
+}
+
+/// 透明度命令
+pub struct FadeCommand {
+    common: CommonCommand,
+    start: f64,
+    end: f64,
+}
+
+/// 移动命令
+pub struct MoveCommand {
+    common: CommonCommand,
+    start: Point,
+    end: Point,
+}
+pub struct MoveXCommand {
+    common: CommonCommand,
+    start: i64,
+    end: i64,
+}
+pub struct MoveYCommand {
+    common: CommonCommand,
+    start: i64,
+    end: i64,
+}
+
+/// 缩放命令
+pub struct ScaleCommand {
+    common: CommonCommand,
+    start: f64,
+    end: f64,
+}
+
+/// 矢量缩放命令
+pub struct ScaleVectorCommand {
+    common: CommonCommand,
+    start: Point,
+    end: Point,
+}
+
+/// 旋转命令
+pub struct RotateCommand {
+    common: CommonCommand,
+    start: f64,
+    end: f64,
+}
+
+/// 颜色命令
+pub struct ColorCommand {
+    common: CommonCommand,
+    start: Color,
+    end: Color,
+}
+
 /// 参数命令
-pub struct ParameterCommand {}
+pub struct ParameterCommand {
+    common: CommonCommand,
+    parameter: ParameterCommandType,
+}
 
 /// 循环命令
-pub struct LoopCommand {}
+pub struct LoopCommand {
+    start_time: i64,
+    loop_count: i64,
+}
 
 macro_rules! bind_type {
     ($($name:ident => $type_name:expr,)+) => {$(
@@ -50,6 +96,7 @@ bind_type! {
     MoveXCommand => "MX",
     MoveYCommand => "MY",
     ScaleCommand => "S",
+    ScaleVectorCommand => "V",
     RotateCommand => "R",
     ColorCommand => "C",
     ParameterCommand => "P",
